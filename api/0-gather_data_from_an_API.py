d02ce476c012d10f671dc820e29aa55ce9e59da3
Meghan-ke <m.mwizerwa@alustudent.com>
2025-10-19 21:40:02 +0000

    user api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,47 +1,23 @@
 #!/usr/bin/python3
 
-""" 
-this script is to use an api to extract employee details
-"""
+"""gather data from an api"""
 import requests
 import sys
-def main():
-   """ fetch and display employee todo progress."""
-if len(sys.argv) !=2:
-   print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
 
-try:
-   employee_id = int(sys.argv[1])
-except ValueError:
-   print("Employee id must an integer")
-   sys.exit(1)
+if __name__ == '__main__':
+    """gather data from an api"""
+    response_todo = requests.get(
+        "https://jsonplaceholder.typicode.com/user/" + sys.argv[1] + "/todos"
+    )
+    data = response_todo.json()
+    completed_task = []
+    for i in data:
+        if i['completed']:
+            completed_task.append(i['title'])
 
-base_url = "https://jsonplaceholder.typicode.com"
-
-#now the user info
-
-user_url = f"{base_url}/users/{employee_id}"
-user_response = requests.get(user_url)
-if user_response.status_code !=200:
-   print("Employee not found.")
-   sys.exit(1)
-user_data = user_response.json()
-employee_name= user_data.get("name")
-
-#now let us get the todo list
-
-todos_url = F"{base_url}/todos"
-todos_response = requests.get(todos_url, params={"userId": employee_id})
-todos = todos_response.json()
-
-# find the completed tasks
-completed_tasks = [task for task in todos if task.get("completed")]
-
-# the output
-print(f"Employed {employee_name} is done with tasks({len(done_tasks)}/{len(todos)}):")
-for task in completed_tasks:
-   print(f"\t {task.get('title')}")
-
-if __name__ == "__main__":
-   main()
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+    ).json()
+    print("Employee {} is done with tasks({}/{}):".
+          format(user["name"], len(completed_task), len(data)))
+    [print("\t " + task) for task in completed_task]
